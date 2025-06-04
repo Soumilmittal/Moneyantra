b8fd6a11f413364e26eca57fb28cf8f1acc4a3d5
Aryesh02 <aryeshsrivastava@gmail.com>
2025-06-04 10:54:10 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-amber-400'>
+      Moneyantra
+    </div>
+  )
+}
+
+export default App
