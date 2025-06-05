9bb5cd272ec0b4d212903d4d6f3ac41fbe43f0be
ARYANSH Chauhan <146801011+aryansh2616@users.noreply.github.com>
2025-06-05 16:35:22 +0530

    get in touch and footer updated

--- frontend/src/pages/landing_page/Homepage.jsx
+++ frontend/src/pages/landing_page/Homepage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar.jsx";
 import Image from "../../components/Image.jsx";
 import Footer from "../../components/Footer.jsx";
 import About from "../../components/About.jsx";
+import Getintouch from "../../components/getintouch.jsx";
 
 function Homepage() {
     return (
@@ -11,6 +12,7 @@ function Homepage() {
                 <Navbar />
                 <Image />
                 <About />
+                <Getintouch/>
                 <Footer />
             </div>
 
