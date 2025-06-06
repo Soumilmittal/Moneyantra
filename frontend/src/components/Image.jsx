c14fb98d9de55956f68cef2bed23d8b99833496f
Soumilmittal <mittasoumill@gmail.com>
2025-06-06 12:59:37 +0530

    modified:   frontend/src/App.css 	modified:   frontend/src/components/Image.jsx

--- frontend/src/components/Image.jsx
+++ frontend/src/components/Image.jsx
@@ -6,9 +6,12 @@ function Image() {
         <div className="w-screen h-screen relative img-container">
             <img src={img1} alt="background" className="w-full h-[100%] object-cover img" />
             <div className="absolute top-30 left-30 overlay">
-                <h1 className="text-[#33658A]">MONEY<span className="text-[#F26419]">ANTRA</span></h1>
-                <p>Earn More To Spend More</p>
-            </div>        
+                <h1>
+                    <span className="text-[#33658A]">MONEY</span>
+                    <span className="text-[#F26419]">ANTRA</span>
+                </h1>
+                <p className="font-bold text-center">Earn More To Spend More</p>
+            </div>
         </div>
     );
 }
@@ -16,11 +19,11 @@ function Image() {
 export default Image;
 
 
- // <div className="row img" >
-        //     <img src='media/images/img.png' alt="image"></img>
-        //     <div className="overlay">
-        //         <h1><span className="first"> MONEY</span><span className="second">ANTRA</span></h1>
-        //         <p>Earn More To Spend More</p>
-        //     </div>
+// <div className="row img" >
+//     <img src='media/images/img.png' alt="image"></img>
+//     <div className="overlay">
+//         <h1><span className="first"> MONEY</span><span className="second">ANTRA</span></h1>
+//         <p>Earn More To Spend More</p>
+//     </div>
 
-        // </div>
+// </div>
