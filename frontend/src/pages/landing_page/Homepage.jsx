034f75aa81a82589a60a165a6a1bbdd10741c4f0
Soumilmittal <mittasoumill@gmail.com>
2025-06-27 16:53:45 +0530

    Tax Management page with grid added

--- frontend/src/pages/landing_page/Homepage.jsx
+++ frontend/src/pages/landing_page/Homepage.jsx
@@ -3,11 +3,13 @@ import Navbar from "../../components/Navbar.jsx";
 import Image from "../../components/Image.jsx";
 import Footer from "../../components/Footer.jsx";
 import About from "../../components/About.jsx";
+import TaxManagement from "../../components/TaxManagement.jsx";
 
 function Homepage() {
     return (
         <>
             <div className="parent">
+                <TaxManagement />
                 <Navbar />
                 <Image />
                 <About />
